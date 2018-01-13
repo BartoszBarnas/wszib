737b0889911e22f4d4cd6f6630581b989ebebf42
babarnas <babarnas@KOMP409-16>
2018-01-13 15:11:34 +0100

    Producs list added

--- src/Shop.Web/Controllers/ProductsController.cs
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Core.Domain;
+using Shop.Web.Models;
 
 namespace Shop.Web.Controllers
 {
+    [Route("products")]
     public class ProductsController : Controller
     {
 
@@ -18,10 +20,31 @@ namespace Shop.Web.Controllers
 
         };
 
-
+        [HttpGet]
         public IActionResult Index()
         {
             return View(_products);
+        }
+
+
+        [HttpGet("add")]
+        public IActionResult Add()
+        {
+            var viewModel = new ProductViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add (ProductViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            _products.Add(new Product(viewModel.Name, viewModel.Category, viewModel.Price));
+            return RedirectToAction(nameof(Index));
         }
     }
 }
